1c26860aa58675d712e3143a4e74291e6dcf999a
TalhaFatur74 <148361554+TalhaFatur74@users.noreply.github.com>
2025-01-07 13:21:49 +0700

    Update SubKategoriDAO.java

--- src/main/java/dao/SubKategoriDAO.java
+++ src/main/java/dao/SubKategoriDAO.java
@@ -1,55 +1,103 @@
 package dao;
 
-import static dao.BaseDAO.getCon;
-import java.util.Scanner;
-import model.SubKategori;
+import model.Category;
+import model.Stone;
+import model.SubCategory;
+
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.UUID;
 
-public class SubKategoriDAO extends BaseDAO {
+import static java.util.UUID.fromString;
+
+public class SubCategoryDAO extends BaseDAO {
 
     private static PreparedStatement st;
     private static Connection con;
 
-    public static void displaySubCategories(Scanner sc, int parentCategoryId) {
+    public static ArrayList<SubCategory> getSubCategories(String categoryName)
+    {
+        SubCategory subCategory;
+        ArrayList<SubCategory> subCategories = new ArrayList<>();
+        ArrayList<Stone> stones;
 
-        List<SubKategori> kategoriList = new ArrayList<>();
-
-        while (true) {
-            System.out.println("\n==== SUB-KATEGORI ====");
-
-            try {
-                con = getCon();
-                String query = String.format("SELECT idSubKategoriBatu, "
-                        + "namaSubKategori FROM sub_kategori WHERE idKategoriBatu = %d", parentCategoryId);
-
-                st = con.prepareStatement(query);
-                ResultSet resultSet = st.executeQuery();
-
-                System.out.println("\n====SUB-KATEGORI====");
-                int count = 1;
-                while (resultSet.next()) {
-                    SubKategori kategori = new SubKategori(resultSet.getString("namaSubKategori"));
-                    kategoriList.add(kategori);
-                    System.out.println(count + ". " + resultSet.getString("namaSubKategori") + " (ID:" + resultSet.getInt("idSubKategoriBatu") + ")");
-                    count++;
-                }
-
-                System.out.println("0. Kembali ke Kategori Utama");
-                System.out.print("Pilih ID: ");
-                int subCategoryId = sc.nextInt();
-                sc.nextLine();
-
-                if (subCategoryId == 0) {
-                    break;
-                } else {
-                    BatuDAO.displayBatu(sc, subCategoryId);
-                }
-
-            } catch (SQLException e) {
-                e.printStackTrace();
+        try
+        {
+            con = getCon();
+            String query = "SELECT namaSubKategori\n" +
+                           "FROM sub_kategori\n" +
+                           "INNER JOIN kategori ON sub_kategori.idKategoriBatu = kategori.idKategoriBatu\n" +
+                           "WHERE kategori.namaKategori = '%s'";
+            query = String.format(query, categoryName);
+            st = con.prepareStatement(query);
+            ResultSet resultSet = st.executeQuery();
+            while (resultSet.next())
+            {
+                stones = StoneDAO.getListOfStones(resultSet.getString("namaSubKategori"));
+                subCategory = new SubCategory(resultSet.getString("namaSubKategori"), stones);
+                subCategories.add(subCategory);
             }
         }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            closeCon(con);
+        }
+        return subCategories;
+    }
+
+    public static UUID getSubCategoryId(String subCategoryName)
+    {
+        UUID subCategoryID = null;
+        try
+        {
+            con = getCon();
+            String query = "SELECT idSubKategoriBatu FROM sub_kategori WHERE namaSubKategori = '%s'";
+            query = String.format(query, subCategoryName);
+            st = con.prepareStatement(query);
+            ResultSet resultSet = st.executeQuery();
+            if (resultSet.next())
+            {
+                subCategoryID = fromString(resultSet.getString("idSubKategoriBatu"));
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            closeCon(con);
+        }
+        return subCategoryID;
+    }
+
+    public static String getSubCategoryByID(UUID subCategoryID)
+    {
+        String subCategory = null;
+        try
+        {
+            con = getCon();
+            String query = "SELECT namaSubKategori FROM sub_kategori WHERE idSubKategoriBatu = '%s'";
+            query = String.format(query, subCategoryID);
+            st = con.prepareStatement(query);
+            ResultSet resultSet = st.executeQuery();
+            if (resultSet.next())
+            {
+                subCategory = resultSet.getString("namaSubKategori");
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            closeCon(con);
+        }
+        return subCategory;
     }
 }
