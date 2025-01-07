c6c5b2c9a742be6df6966cdc9091344881d26232
TalhaFatur74 <148361554+TalhaFatur74@users.noreply.github.com>
2025-01-07 13:20:40 +0700

    Update KategoriDAO.java

--- src/main/java/dao/KategoriDAO.java
+++ src/main/java/dao/KategoriDAO.java
@@ -1,56 +1,46 @@
 package dao;
 
-import java.util.Scanner;
-import model.Kategori;
+import model.Category;
+import model.SubCategory;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 
-public class KategoriDAO extends BaseDAO {
+public class CategoryDAO extends BaseDAO {
 
     private static PreparedStatement st;
     private static Connection con;
 
-    public static void displayCategories(Scanner sc) {
-
-        List<Kategori> kategoriList = new ArrayList<>();
-
-        while (true) {
-
-            try {
-                con = getCon();
-                String query = "SELECT idKategoriBatu, namaKategori FROM kategori";
-
-                st = con.prepareStatement(query);
-                ResultSet resultSet = st.executeQuery();
-
-                System.out.println("Kategori Utama:");
-                while (resultSet.next()) {
-
-                    Kategori kategori = new Kategori(resultSet.getString("namaKategori"));
-                    kategoriList.add(kategori);
-                    System.out.println(resultSet.getInt("idKategoriBatu") + ". " + resultSet.getString("namaKategori"));
-                }
-
-                System.out.println("0. Kembali ke Kategori Utama");
-                System.out.print("Pilih: ");
-                int subCategoryId = sc.nextInt();
-                sc.nextLine();
-
-                if (subCategoryId == 0) {
-                    break;
-                } else {
-                    SubKategoriDAO.displaySubCategories(sc, subCategoryId);
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
+    public static ArrayList<Category> getAllCategories()
+    {
+        Category category;
+        ArrayList<Category> categories = new ArrayList<>();
+        ArrayList<SubCategory> subCategories;
+        try
+        {
+            con = getCon();
+            String query = "SELECT namaKategori FROM kategori";
+            st = con.prepareStatement(query);
+            ResultSet resultSet = st.executeQuery();
+            while (resultSet.next())
+            {
+                subCategories = SubCategoryDAO.getSubCategories(resultSet.getString("namaKategori"));
+                category = new Category(resultSet.getString("namaKategori"), subCategories);
+                categories.add(category);
             }
-
         }
-
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            closeCon(con);
+        }
+        return categories;
     }
 
 }
